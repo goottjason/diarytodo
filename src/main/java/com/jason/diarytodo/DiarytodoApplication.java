f722fd0683019d35d973f7e3ea57444ab6187f03
goottjason <goottjason@gmail.com>
2025-05-14 01:06:13 +0900

    first

---
+++ src/main/java/com/jason/diarytodo/DiarytodoApplication.java
@@ -0,0 +1,13 @@
+package com.jason.diarytodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiarytodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiarytodoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jason/diarytodo/DiarytodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.diarytodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiarytodoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
